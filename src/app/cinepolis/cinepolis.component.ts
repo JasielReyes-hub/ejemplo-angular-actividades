a88d5334fe60a952b891d65ac8818d8b3838d3d1
JasielReyes-hub <19002495@alumnos.utleon.edu.mx>
2022-10-04 21:23:26 -0500

    Se subio ya con las correcciones el proyecto de cinepolis

--- src/app/cinepolis/cinepolis.component.ts
+++ src/app/cinepolis/cinepolis.component.ts
@@ -12,6 +12,9 @@ export class CinepolisComponent {
   comprados: string = '';
   PagoTarjeta: string = '';
   BoletosCantidad: string = '';
+  Aviso: string;
+  MensajeActivar: boolean;
+
 
 
   
@@ -23,39 +26,62 @@ export class CinepolisComponent {
   Namecomprador: string = '';
   porcentaje15: number = 0;
   porcentaje10: number = 0;
+  activar1:boolean;
 
   constructor() {}
 
   procesar(): void {
+
+    //Operaciones a realizar
     this.costoU = parseInt(this.BoletosCantidad) * 12;
     this.boletosP = parseInt(this.comprados) * 7;
     this.boletosPer = parseInt(this.BoletosCantidad);
 
-   
+   //Porcentajes a realizar
     this.porcentaje10 = (this.costoU * 10) / 100;
     this.porcentaje15 = (this.costoU * 15) / 100;
+
+
 
     if (this.boletosPer <= this.boletosP) {
       if (
         this.BoletosCantidad == '3' ||
         this.BoletosCantidad == '4' ||
-        this.BoletosCantidad == '5'
+        this.BoletosCantidad == '5' 
+        
       ) {
+        this.activar1=true;
+        this.MensajeActivar = false;
         this.Namecomprador = this.comprador1;
         this.PrecioTotal = this.costoU - this.porcentaje10;
+        this.Aviso = '';
+
       } else if (this.BoletosCantidad == '1' || this.BoletosCantidad == '2') {
+        this.MensajeActivar = false;
+        this.activar1=true;
         this.Namecomprador = this.comprador1;
         this.PrecioTotal = this.costoU;
+        this.Aviso = '';
       } else {
+        this.MensajeActivar = false;
+        this.activar1=true;
         this.Namecomprador = this.comprador1;
         this.PrecioTotal = this.costoU - this.porcentaje15;
+        this.Aviso = '';
       }
+      
       if (this.PagoTarjeta == 'si') {
-        this.PrecioTotal = this.PrecioTotal - this.porcentaje10;
+        this.PrecioTotal = this.PrecioTotal - (this.PrecioTotal * .10);
       } 
       if (this.PagoTarjeta == 'no') {
         this.PrecioTotal
       } 
+
+    }
+    else{
+      this.Aviso= ("Recuerda que solo se puede comprar 7 boletos por persona");
+      this.MensajeActivar = true;
+      this.activar1=false;
     }
   }
 }
